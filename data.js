8aaf86bbdf456d8c9cad4ff4ea752a8ea10b8485
moustudiointerior <144911346+moustudiointerior@users.noreply.github.com>
2023-10-02 12:18:02 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,113 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-parter-21",
+      "name": "PARTER 2.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 700,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4419751924817845,
+          "pitch": 0.1018236059121147,
+          "rotation": 0,
+          "target": "1-parter-22"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-parter-22",
+      "name": "PARTER 2.2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 700,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0049827301219416,
+          "pitch": 0.10925172954484808,
+          "rotation": 0,
+          "target": "2-parter-23"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-parter-23",
+      "name": "PARTER 2.3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 700,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.725994208449853,
+          "pitch": 0.08747909849555846,
+          "rotation": 0,
+          "target": "0-parter-21"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
